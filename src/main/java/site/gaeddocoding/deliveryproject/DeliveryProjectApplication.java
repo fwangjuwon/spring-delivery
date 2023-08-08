238e6c4727870672bd40849921675b1d31b2ee9a
fwangjuwon <fwangjuwon@naver.com>
2023-08-08 20:22:07 +0900

    first commit

---
+++ src/main/java/site/gaeddocoding/deliveryproject/DeliveryProjectApplication.java
@@ -0,0 +1,14 @@
+package site.gaeddocoding.deliveryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryProjectApplication.class, args);
+	}
+
+
+}
