2800eb71a2b10a21d6e8b0ec4f608035e225e768
fwangjuwon <fwangjuwon@naver.com>
2023-08-10 17:40:38 +0900

    도메인 생성 - food

--- src/main/java/site/gaeddocoding/deliveryproject/DeliveryProjectApplication.java
+++ src/main/java/site/gaeddocoding/deliveryproject/DeliveryProjectApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeliveryProjectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DeliveryProjectApplication.class, args);
 	}
-
-
 }
---
+++ src/main/java/site/gaeddocoding/deliveryproject/domain/food/Food.java
@@ -0,0 +1,12 @@
+package site.gaeddocoding.deliveryproject.domain.food;
+
+
+public class Food {
+
+    private Integer id;
+    private String foodName;
+    private Integer foodPrice;
+    private Integer foodAmt;
+    private String foodSize;
+    private boolean sideDish;
+}
