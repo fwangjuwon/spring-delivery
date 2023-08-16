7b164ee2e5b7c2868e526966d86ec146cd0e27da
fwangjuwon <fwangjuwon@naver.com>
2023-08-16 20:01:47 +0900

    컨트롤러 api생성 완료

--- src/main/java/site/gaeddocoding/deliveryproject/food/FoodDto.java
+++ src/main/java/site/gaeddocoding/deliveryproject/food/FoodDto.java
@@ -1,4 +1,13 @@
 package site.gaeddocoding.deliveryproject.food;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class FoodDto {
+    private Integer code; //-1 실패, 1 성공
+    private String msg; //에러메세지
 }
--- src/main/java/site/gaeddocoding/deliveryproject/food/FoodRepository.java
+++ src/main/java/site/gaeddocoding/deliveryproject/food/FoodRepository.java
@@ -1,4 +1,8 @@
 package site.gaeddocoding.deliveryproject.food;
 
-public interface FoodRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long> {
 }
