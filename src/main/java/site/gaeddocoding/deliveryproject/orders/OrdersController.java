c2a4baa24739c2063b62c344de1161fcac32937d
fwangjuwon <fwangjuwon@naver.com>
2023-09-01 14:46:06 +0900

    주문(orders) 컨트롤러, 서비스, 레파지토리 틀 구현

--- src/main/java/site/gaeddocoding/deliveryproject/orders/OrdersController.java
+++ src/main/java/site/gaeddocoding/deliveryproject/orders/OrdersController.java
@@ -2,7 +2,36 @@ package site.gaeddocoding.deliveryproject.orders;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class OrdersController {
+
+    private final OrdersService ordersService;
+
+    // 주문 생성 (post)
+    public String insertOrders() {
+        ordersService.doInsertOrders();
+        return "";
+    }
+
+    // 주문 변경 (put)
+    public String updateOrders() {
+        ordersService.doUpateOrders();
+        return "";
+    }
+
+    // 주문 취소 (delete)
+    public String cancelOrders() {
+        ordersService.doCancelOrders();
+        return "";
+    }
+
+    // 주문 조회 (get)
+    public String searchOrders() {
+        ordersService.doSearchOrders();
+        return "";
+    }
 
 }
--- src/main/java/site/gaeddocoding/deliveryproject/orders/OrdersService.java
+++ src/main/java/site/gaeddocoding/deliveryproject/orders/OrdersService.java
@@ -1,8 +1,34 @@
 package site.gaeddocoding.deliveryproject.orders;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class OrdersService {
+
+    private final OrdersRepository ordersRepository;
+
+    @Transactional
+    public void doInsertOrders() {
+
+    }
+
+    @Transactional
+    public void doUpateOrders() {
+
+    }
+
+    @Transactional
+    public void doCancelOrders() {
+
+    }
+
+    public void doSearchOrders() {
+
+    }
 
 }
